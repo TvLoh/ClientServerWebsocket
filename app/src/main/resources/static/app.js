b156c73183941a7798939184d96c828e5377bdb4
Kozina, Ante <Ante.Kozina@adesso.de>
2020-11-06 15:31:02 +0100

    updated

--- app/src/main/resources/static/app.js
+++ app/src/main/resources/static/app.js
@@ -18,7 +18,7 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
+        stompClient.subscribe('/topic/session', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
     });
@@ -33,7 +33,7 @@ function disconnect() {
 }
 
 function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+    stompClient.send("/app/id", {}, JSON.stringify({'id': $("#name").val()}));
 }
 
 function showGreeting(message) {
